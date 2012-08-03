427befcd1f2c16ab82d3de523ca4736aeda9e88b
Rob Nichols <git@nicholshayes.co.uk>
2012-08-03 15:32:17 +0100

    Adds ability to create events on Google calendar

--- web.rb
+++ web.rb
@@ -8,14 +8,34 @@ require_relative 'lib/event_calendar'
 module EventCalendar
 
   get '/' do
+    @event = Event.new
+    @events = @event.calendar.events
+    @events = [@events] unless @events.kind_of? Array
     erb :event
   end
 
   post '/' do
     @event = Event.new(params)
+    @event.save
+    @events = @event.calendar.events
+    @events = [@events] unless @events.kind_of? Array
     erb :event
   end
-
+  
+  get '/event' do
+    @event = Event.find(params[:id])
+    erb :event
+  end
+  
+  post '/event' do
+    @event = Event.find(params[:id])
+    @event.load(params)
+    @event.save
+    @events = @event.calendar.events
+    @events = [@events] unless @events.kind_of? Array
+    erb :event
+  end
+ 
 end
 
 
